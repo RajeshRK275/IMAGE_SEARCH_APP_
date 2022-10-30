137828ddcf435c9fe143eca62d1e78d8e1bacf07
Rajeshkumar <rajesheswaran27599@gmail.com>
2022-10-31 04:15:39 +0530

    first commit

---
+++ src/components/image.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class Test extends React.Component {
+    render(){
+        return (
+            <img src={this.props.src}/>
+        );
+    }
+}
+
+export default Test;
